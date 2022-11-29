09d42b72ba91d03a8d50547f8ffe57d13b3fba94
Niranjan Jangir <108334402+whennijcodes@users.noreply.github.com>
2022-11-29 21:03:59 +0530

    Add files via upload
    Talks about primitive data types and object.

---
+++ primitive data.js
@@ -0,0 +1,28 @@
+// primitive data types in JS
+/* N umber
+N ull
+S ymbol 
+S tring
+B oolean
+B igint 
+U ndefined */
+
+let a  = null;
+let b = 40;
+let c = true;
+let d = BigInt("23");
+let e = "string";
+let f = Symbol("it is a symbol");
+let g
+console.log(a,b,c,d,e,f,g);
+console.log(typeof c);
+
+
+//objects:
+const firstObject={
+    "Raju" : true,
+    "Kaju" : false,
+    "Lovinsh" : undefined
+}
+
+console.log(firstObject["Raju"])

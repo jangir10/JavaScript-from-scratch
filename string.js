24da782201bf5547c75dcc2d4ba8182ed96a381c
Niranjan Jangir <108334402+whennijcodes@users.noreply.github.com>
2022-12-01 22:36:23 +0530

    Add files via upload
    Added file for JS strings, Template Literals

---
+++ string.js
@@ -0,0 +1,12 @@
+let name = "Niranjan"
+console.log(name.length)
+
+//Template Literals:
+let boy1 = "Ramesh"
+let boy2 = "Sudheer" 
+let sentence = `${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+//  
+let fruit = 'banana\ts banana'
+console.log(fruit)

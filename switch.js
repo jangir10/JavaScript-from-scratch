09e169195c0f2141aecc636b456c540cf6e37afc
Niranjan Jangir <108334402+whennijcodes@users.noreply.github.com>
2022-12-01 19:44:03 +0530

    Add files via upload
    Added file for js switch case. Read more at mdn docs.

---
+++ switch.js
@@ -0,0 +1,15 @@
+const expression = "Apple";
+
+switch(expression) { 
+
+    case "sev":
+        console.log("Hindi");
+        break;
+    case "Apple":
+        console.log("English");
+        break;       
+    default:
+        console.log("are kahna kya chahte ho?")
+        
+        
+}

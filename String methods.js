811510b919824eef315ad77b72e56950d774f4f7
Niranjan Jangir <108334402+whennijcodes@users.noreply.github.com>
2023-01-02 14:46:39 +0530

    Add files via upload
    String methods uploaded

---
+++ String methods.js
@@ -0,0 +1,7 @@
+let str = "When nij codes nij codes";
+console.log(str.length);
+console.log(str.indexOf("nij"));
+console.log(str.lastIndexOf("nij"));
+console.log(str.slice(0,5));
+console.log(str.replace("nij", "niranjan"));
+//refer to mdn docs

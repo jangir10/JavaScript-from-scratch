8928c1a6d5f050ac116b6118c3f6995d28492536
Niranjan Jangir <108334402+whennijcodes@users.noreply.github.com>
2023-01-02 14:47:01 +0530

    Add files via upload
    array methods added.

---
+++ Array Methods.js
@@ -0,0 +1,19 @@
+let arr = [1 ,4 , "Nir", null, true];
+//length:
+// console.log(arr.length);
+//removing last element:
+// arr.pop();
+//adding an element at the end:
+// arr.push("Niranjan");
+//removing first ele:
+// arr.shift();
+// console.log(arr);
+//adding ele at beginning:
+// arr.unshift("Whennijcodes");
+// console.log(arr)
+// [ 'Whennijcodes', 1, 4, 'Nir', null, true ]
+// d = arr.unshift("Whennijcodes");
+// console.log(d) //gives the new length
+console.log(arr.toString()); //converts eles into string
+console.log(arr.sort()) // sorts alphabetically.
+//refer to mdn docs.

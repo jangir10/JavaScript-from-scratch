a0753a98a9c5a10a9fc7cb8bc2f262f2a29e7dc7
Niranjan Jangir <108334402+whennijcodes@users.noreply.github.com>
2023-01-02 14:52:17 +0530

    Add files via upload
    Date added.

---
+++ Date.js
@@ -0,0 +1,11 @@
+let date = new Date();
+console.log(date);
+console.log(date.getTime());//time in seconds
+console.log(date.getFullYear());
+console.log(date.getDay());
+console.log(date.getMinutes());
+console.log(date.getHours());
+
+//refer the internet.
+
+
